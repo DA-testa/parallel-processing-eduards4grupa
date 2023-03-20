2a8a3550a7f01937bf678fc7902595cd048bbf50
eduards4grupa <125820885+eduards4grupa@users.noreply.github.com>
2023-03-20 02:49:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def parallel_processing(n, m, data):
             for j in range(len(threads)):
                 if threads[j][1]<threads[maz][1]:
                     maz=j
-        output.append([threads[maz][0],threads[maz][0])
+        output.append([threads[maz][0],threads[maz][0]])
         threads[maz][1]+=int(data[i])
         
             
